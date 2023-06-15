ea900bec9c10ba4af321fdcd344c0b92d13b4a63
Nastya880 <oidmakeevy@gmail.com>
2023-06-15 11:24:31 +0300

    добавила верстку на мобильном

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nav_list_mob = document.querySelector(".nav-list-mob")
+let btn_menu = document.querySelector("#btn-open-close-menu")
+
+btn_menu.addEventListener("click", (e) => {
+  e.preventDefault();
+  // смена темы
+  nav_list_mob.classList.toggle("list-mob-show"); 
+  btn_menu.classList.toggle("btn-open-menu");
+  btn_menu.classList.toggle("btn-close-menu");
+});
